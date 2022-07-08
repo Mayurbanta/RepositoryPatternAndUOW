0301f01dfd2c770d68e1187b076362b48563a820
Mayur Banta <mbanta@nextgen.com>
2022-07-08 13:28:14 +0530

    Added Customers Controller

---
+++ RepositoryPatternUOW/Controllers/CustomersController.cs
@@ -0,0 +1,68 @@
+﻿using Engine;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RepositoryPatternUOW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomersController(ILogger<WeatherForecastController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            this._unitOfWork = unitOfWork;
+        }
+
+
+        // GET: api/<CustomersController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var customers = _unitOfWork.CustomerRepository.Get(null,null,"");
+
+            return Ok(customers);
+        }
+
+        // GET api/<CustomersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
